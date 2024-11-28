7778391a87d4c9d30c476dc341348312b855372f
mhsiyaz <mhsiyaz49@gmail.com>
2024-11-29 00:05:32 +0530

    module crud operations done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Helpers\ResponseHelper;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\PortalUserController;
 use App\Models\PortalUser;
 use Illuminate\Http\Request;
@@ -18,13 +19,20 @@ use Illuminate\Support\Facades\Route;
         return response()->json(['token' => $token], 200);
     });
 
+    Route::get('/v1/modules',[ModuleController::class,'showAllModules']);
+    Route::get('/v1/modules/{id}',[ModuleController::class,'getModuleById']);
+    Route::post('/v1/modules',[ModuleController::class,'addModule']);
+    Route::patch('/v1/modules/{id}',[ModuleController::class,'updateModuleById']);
+    Route::delete('/v1/modules/{id}',[ModuleController::class,'deleteModuleById']);
+
+
     Route::post('/login',[AuthController::class,'login']);
     Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
     Route::post('/register',[AuthController::class,'register']);
-    //Route::get('v1/users',[PortalUserController::class,'all'])->middleware('auth:sanctum');
+
 
 // Routes are beginning with /v1
-Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
+Route::prefix('v1')->group(function () {
     Route::get("/", function() {
         return ResponseHelper::success("Welcome to API ".config('app.version'));
     });
