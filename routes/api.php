b8c2c88553ed1da3c4e4c924687cb38a445c3c17
RasuwanK <kalharaweragala@gmail.com>
2025-01-05 20:52:56 +0530

    Regrouped routes

--- routes/api.php
+++ routes/api.php
@@ -25,71 +25,91 @@ Route::prefix('api')->group(function () {
         return response()->json(['token' => $token], 200);
     });
 
-    Route::prefix('/v1/modules')->group(function () {
-        Route::get('/', [ModuleController::class, 'showAllModules']);
-        Route::get('/{id}', [ModuleController::class, 'getModuleById']);
-        Route::post('/', [ModuleController::class, 'addModule']);
-        Route::patch('/{id}', [ModuleController::class, 'updateModuleById']);
-        Route::delete('/{id}', [ModuleController::class, 'deleteModuleById']);
-        Route::get('/{id}/activities', [ModuleController::class, 'getAllActivitiesForAModule']);
-        Route::get('{id}/activities/assignment', [ModuleController::class, 'getAllAssignments']);
-        Route::get('{id}/activities/quiz', [ModuleController::class, 'getAllQuizes']);
-        Route::get('/{id}/activities/{activityid}', [ModuleController::class, 'getActivity']);
-        Route::patch('/{id}/activities/{activityid}', [ModuleController::class, 'updateActivity']);
-        Route::post('/{id}/activities/assignment', [ModuleController::class, 'addAssignment']);
-        Route::post('/{id}/activities/quiz', [ModuleController::class, 'addQuiz']);
-        Route::delete('/{id}/activities/{activityid}', [ModuleController::class, 'deleteActivity']);
-        Route::post('/{id}/announcements', [ModuleController::class, 'createAnnouncement']);
-        Route::get('/{id}/announcements', [ModuleController::class, 'getAnnouncements']);
+    Route::prefix('/v1')->group(function () {
+
+        // Modules related
+        Route::prefix('/modules')->group(function () {
+            Route::get('/', [ModuleController::class, 'showAllModules']);
+            Route::get('/{id}', [ModuleController::class, 'getModuleById']);
+            Route::post('/', [ModuleController::class, 'addModule']);
+            Route::patch('/{id}', [ModuleController::class, 'updateModuleById']);
+            Route::delete('/{id}', [ModuleController::class, 'deleteModuleById']);
+            Route::get('/{id}/activities', [ModuleController::class, 'getAllActivitiesForAModule']);
+            Route::get('{id}/activities/assignment', [ModuleController::class, 'getAllAssignments']);
+            Route::get('{id}/activities/quiz', [ModuleController::class, 'getAllQuizes']);
+            Route::get('/{id}/activities/{activityid}', [ModuleController::class, 'getActivity']);
+            Route::patch('/{id}/activities/{activityid}', [ModuleController::class, 'updateActivity']);
+            Route::post('/{id}/activities/assignment', [ModuleController::class, 'addAssignment']);
+            Route::post('/{id}/activities/quiz', [ModuleController::class, 'addQuiz']);
+            Route::delete('/{id}/activities/{activityid}', [ModuleController::class, 'deleteActivity']);
+            Route::post('/{id}/announcements', [ModuleController::class, 'createAnnouncement']);
+            Route::get('/{id}/announcements', [ModuleController::class, 'getAnnouncements']);
+        });
+
+        // Announcements related
+        Route::prefix('/announcements')->group(function () {
+            Route::patch('/{announcementid}', [ModuleController::class, 'updateAnnouncement']);
+            Route::delete('/{announcementid}', [ModuleController::class, 'deleteAnnouncement']);
+            Route::post('/{announcementid}/answers', [AnnouncementController::class, 'addAnswer']);
+            Route::get('/{announcementid}/answers', [AnnouncementController::class, 'getAnswers']);
+            Route::patch('/{announcementid}/answers/{answerid}', [AnnouncementController::class, 'updateAnswer'])->middleware('auth:sanctum');
+            Route::delete('/{announcementid}/answers/{answerid}', [AnnouncementController::class, 'deleteAnswer'])->middleware('auth:sanctum');
+        });
+
+        // Activities related
+        Route::prefix('/activities')->group(function () {
+            Route::post('/{activityId}/submit', [ParticipateController::class, 'submitActivity']);
+            Route::get('{activityId}/submissions', [ParticipateController::class, 'fetchSubmissions']);
+            Route::patch('/{activityId}/submissions/{userId}/grade', [ParticipateController::class, 'gradeSubmission']);
+        });
+
+        Route::prefix('/modules')->group(function () {
+            Route::post('/{moduleid}/topics', [ModuleController::class, 'addTopic']);
+            Route::get('/{moduleid}/topics', [ModuleController::class, 'getTopics']);
+            Route::patch('/{moduleid}/topic/{topicid}', [ModuleController::class, 'updateTopic']);
+            Route::delete('/{moduleid}/topic/{topicid}', [ModuleController::class, 'deleteTopic']);
+        });
+
+        // Topics related
+        Route::prefix('/topics')->group(function () {
+            Route::post('/{topicid}/materials', [ModuleController::class, 'addLectureMaterial']);
+            Route::get('/{topicid}/materials', [ModuleController::class, 'getLectureMaterials']);
+            Route::patch('/{topicid}/materials/{materialsid}', [TopicController::class, 'updateMaterials']);
+            Route::delete('/{topicid}/materials/{materialsid}', [TopicController::class, 'deleteMaterials']);
+            Route::patch('/{topicid}/toggle-visibility', [TopicController::class, 'toggleVisibility']);
+            Route::patch('/{topicid}/mark-complete', [TopicController::class, 'markAsComplete']);
+        });
+
+        Route::prefix('/quiz')->group(function () {
+
+            Route::post('/{id}/questions', [ActivityController::class, 'addQuestion']);
+            Route::get('/{id}/questions', [ActivityController::class, 'getQuestions']);
+            Route::patch('/{id}/questions/{queid}', [ActivityController::class, 'updateQuestion']);
+            Route::delete('/{id}/questions/{queid}', [ActivityController::class, 'deleteSpecificQuestion']);
+            Route::delete('/{id}/questions', [ActivityController::class, 'deleteAllQuestion']);
+        });
+
+        Route::prefix('/events')->group(function () {
+            // TODO: Need to fix this Route::get('/v1/users/{id}/events', [EventController::class, 'getAllEventsForAUser']);
+            Route::get('events/{eventid}', [EventController::class, 'getSpecificEventDetails']);
+            Route::post('/', [EventController::class, 'createEventForUsers']); //userids will be passed in body
+            // TODO: Need to fix this Route::get('/v1/users/{id}/events/{eventid}', [EventController::class, 'getSpecificEventForAUser']);
+            Route::patch('/{eventid}', [EventController::class, 'updateEvent']);
+            Route::delete('/{eventid}', [EventController::class, 'deleteEvent']);
+        });
+
+        Route::prefix('/courses')->group(function () {
+            Route::get('v1/courses/{id}/modules', [CourseController::class, 'listModules']);
+            Route::post('v1/courses/{id}/modules', [CourseController::class, 'attachModules']);
+            Route::delete('v1/courses/{id}/modules', [CourseController::class, 'detachModules']);
+        });
     });
-    Route::patch('v1/announcements/{announcementid}', [ModuleController::class, 'updateAnnouncement']);
-    Route::delete('v1/announcements/{announcementid}', [ModuleController::class, 'deleteAnnouncement']);
-    Route::post('v1/announcements/{announcementid}/answers', [AnnouncementController::class, 'addAnswer']);
-    Route::get('v1/announcements/{announcementid}/answers', [AnnouncementController::class, 'getAnswers']);
-    Route::patch('v1/announcements/{announcementid}/answers/{answerid}', [AnnouncementController::class, 'updateAnswer'])->middleware('auth:sanctum');
-    Route::delete('v1/announcements/{announcementid}/answers/{answerid}', [AnnouncementController::class, 'deleteAnswer'])->middleware('auth:sanctum');
-
-    Route::post('v1/activities/{activityId}/submit', [ParticipateController::class, 'submitActivity']);
-    Route::get('v1/activities/{activityId}/submissions', [ParticipateController::class, 'fetchSubmissions']);
-    Route::patch('v1/activities/{activityId}/submissions/{userId}/grade', [ParticipateController::class, 'gradeSubmission']);
-
-    Route::post('v1/modules/{moduleid}/topics', [ModuleController::class, 'addTopic']);
-    Route::get('v1/modules/{moduleid}/topics', [ModuleController::class, 'getTopics']);
-    Route::patch('v1/modules/{moduleid}/topic/{topicid}', [ModuleController::class, 'updateTopic']);
-    Route::delete('v1/modules/{moduleid}/topic/{topicid}', [ModuleController::class, 'deleteTopic']);
-
-    Route::post('v1/topics/{topicid}/materials', [ModuleController::class, 'addLectureMaterial']);
-    Route::get('v1/topics/{topicid}/materials', [ModuleController::class, 'getLectureMaterials']);
-    Route::patch('v1/topics/{topicid}/materials/{materialsid}', [TopicController::class, 'updateMaterials']);
-    Route::delete('v1/topics/{topicid}/materials/{materialsid}', [TopicController::class, 'deleteMaterials']);
-    Route::patch('v1/topics/{topicid}/toggle-visibility', [TopicController::class, 'toggleVisibility']);
-    Route::patch('v1/topics/{topicid}/mark-complete', [TopicController::class, 'markAsComplete']);
-
-
-
-    Route::post('/v1/quiz/{id}/questions', [ActivityController::class, 'addQuestion']);
-    Route::get('/v1/quiz/{id}/questions', [ActivityController::class, 'getQuestions']);
-    Route::patch('/v1/quiz/{id}/questions/{queid}', [ActivityController::class, 'updateQuestion']);
-    Route::delete('/v1/quiz/{id}/questions/{queid}', [ActivityController::class, 'deleteSpecificQuestion']);
-    Route::delete('/v1/quiz/{id}/questions', [ActivityController::class, 'deleteAllQuestion']);
-
-    Route::get('/v1/users/{id}/events', [EventController::class, 'getAllEventsForAUser']);
-    Route::get('/v1/events/{eventid}', [EventController::class, 'getSpecificEventDetails']);
-    Route::post('/v1/events', [EventController::class, 'createEventForUsers']); //userids will be passed in body
-    Route::get('/v1/users/{id}/events/{eventid}', [EventController::class, 'getSpecificEventForAUser']);
-    Route::patch('/v1/events/{eventid}', [EventController::class, 'updateEvent']);
-    Route::delete('/v1/events/{eventid}', [EventController::class, 'deleteEvent']);
 
     //    Route::get('/v1/modules',[ModuleController::class,'showAllModules']);
     //    Route::get('/v1/modules/{id}',[ModuleController::class,'getModuleById']);
     //    Route::post('/v1/modules',[ModuleController::class,'addModule']);
     //    Route::patch('/v1/modules/{id}',[ModuleController::class,'updateModuleById']);
     //    Route::delete('/v1/modules/{id}',[ModuleController::class,'deleteModuleById']);
-
-    Route::get('v1/courses/{id}/modules', [CourseController::class, 'listModules']);
-    Route::post('v1/courses/{id}/modules', [CourseController::class, 'attachModules']);
-    Route::delete('v1/courses/{id}/modules', [CourseController::class, 'detachModules']);
-
     Route::prefix('v1')->group(function () {
         Route::post('/signin', [AuthController::class, 'signin']);
         Route::post('/signout', [AuthController::class, 'signout'])->middleware('auth:sanctum');
