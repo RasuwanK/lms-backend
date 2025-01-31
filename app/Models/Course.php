fec5f439618de2b474f84186f4cae3118123c22e
RasuwanK <kalharaweragala@gmail.com>
2025-01-31 14:28:57 +0530

    Removed unwanted fillable field

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Department;
 
 class Course extends Model
 {
@@ -16,15 +17,7 @@ class Course extends Model
         'course_name',
         'credit_value',
         'maximum_students',
-        'department_id'
     ];
-
-    // Define the relationship with the Department
-    public function department()
-    {
-        return $this->belongsTo(Department::class);
-    }
-
     // Define the relationship with Module
 //    public function module()
 //    {
